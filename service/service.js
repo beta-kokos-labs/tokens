5aeb5e3c0ad527d9ea5721cefadf522005629200
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-09-24 23:18:58 +0000

    made code better

---
+++ service/service.js
@@ -0,0 +1,36 @@
+const peer = new Peer();
+let conn;
+
+peer.on('open', (id) => {
+    console.log('My peer ID is: ' + id);
+    alert('Your peer ID is: ' + id);
+});
+
+peer.on('connection', (connection) => {
+    conn = connection;
+    conn.on('data', (data) => {
+        if(data == 'everything-good?'){sendData()}
+        document.getElementById('output').innerText = data;
+    });
+});
+
+function connectPeer() {
+    const peerId = prompt('Enter peer ID:');
+    conn = peer.connect(peerId);
+    conn.on('open', () => {
+        console.log('Connected to: ' + peerId);
+    });
+    conn.on('data', (data) => {
+        document.getElementById('output').innerText = data;
+    });
+}
+
+function sendData() {
+    const data = prompt('how much do you wanna give?');
+    if(data > document.cookie){document.cookie = document.cookie - data
+    if (conn && conn.open) {
+        conn.send(data);
+    } else {
+        alert('Not connected to any peer');
+    }
+}}
---
+++ transfer/transfer.js
@@ -0,0 +1,35 @@
+const peer = new Peer();
+let conn;
+
+peer.on('open', (id) => {
+    console.log('My peer ID is: ' + id);
+    alert('Your peer ID is: ' + id);
+});
+
+peer.on('connection', (connection) => {
+    conn = connection;
+    conn.on('data', (data) => {
+        document.getElementById('output').innerText = data;
+    });
+});
+
+function connectpeer() {
+    const peerId = prompt('Enter peer ID:');
+    conn = peer.connect(peerId);
+    conn.on('open', () => {
+        console.log('Connected to: ' + peerId);
+    });
+    conn.on('data', (data) => {
+        document.cookie += data;
+        alert('transaction complete')
+    });
+}
+
+function sendData() {
+    const data = 'everything-good?';
+    if (conn && conn.open) {
+        conn.send(data);
+    } else {
+        alert('Not connected to any peer');
+    }
+}
